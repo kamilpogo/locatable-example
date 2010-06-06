7079730cddcd8ae8b2fb93a7d3f9ca9ca956b5c7
Github Railwaymen.org <github@railwaymen.org>
2010-06-06 13:20:37 +0200

    initial commit

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '3b93de50a97a33b66f1a7f2bcadb6727f1e97d397767ff188c1e7c062758ed1bccd4bcfb98961ff1866129123b5284fda752083eb1647268a8d0f711a602eaff';
